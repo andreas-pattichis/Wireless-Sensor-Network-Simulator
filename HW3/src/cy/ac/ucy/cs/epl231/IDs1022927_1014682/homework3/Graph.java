975a18940374d2f7fe65f1850e6a1931a28bd767
Kasoulides <ckasou01@ucy.ac.cy>
2020-11-24 20:42:40 +0200

    creating Node

--- HW3/src/cy/ac/ucy/cs/epl231/IDs1022927_1014682/homework3/Graph.java
+++ HW3/src/cy/ac/ucy/cs/epl231/IDs1022927_1014682/homework3/Graph.java
@@ -1,5 +1,27 @@
 package cy.ac.ucy.cs.epl231.IDs1022927_1014682.homework3;
 
+import java.util.LinkedList;
+
+
+
 public class Graph {
+	
+	LinkedList<Node> hashTable;
+	
+	static int hashTableSize;
+	
+	
+	public Graph() {
+		hashTable  = new LinkedList<Node>();
+		hashTableSize = 5;
+		
+	
+	}
+	
+	public int calculateHashkey(String id) {
+		int temp = Integer.parseInt(id);
+		
+		return temp%hashTableSize; 
+	}
 
 }
--- HW3/src/cy/ac/ucy/cs/epl231/IDs1022927_1014682/homework3/Simulation.java
+++ HW3/src/cy/ac/ucy/cs/epl231/IDs1022927_1014682/homework3/Simulation.java
@@ -4,6 +4,18 @@ public class Simulation {
 
 	public static void main(String[] args) {
 		
+		System.out.println("01\t[42, 85]\t30");
+		
+		Coordinates test1 = new Coordinates("[42, 85]");
+		
+		Coordinates test2 = new Coordinates("[30, 60]");
+		
+		System.out.println(test1.calculateDistance(test2));
+		
+		Node sensor = new Node("01\t[42, 85]\t30",4);
+		
+		System.out.println(sensor);
+		
 
 	}
 
